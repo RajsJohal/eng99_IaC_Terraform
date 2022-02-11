3f9248d234c2cc01d95557d125bab72f0e0800cd
RajsJohal <rajsjohal97@hotmail.com>
2022-02-11 17:44:41 +0000

    updated doc

--- README.md
+++ README.md
@@ -10,9 +10,10 @@
 #### Create Folder Strcuture
 
 #### Terraform Script 
-* Created terraform scrit to launch an EC2 instance in AWS using our AWS keys which are located in the environment variables
+* Created terraform script to launch an EC2 instance in AWS using our AWS keys which are located in the environment variables
 * Terraform can identify the keys and use them to create an EC2 within our AWS account 
 * Must ensure we are running terminal as admin in order to provide the neccessary permissions
+* Terrafrom script contains resources which define AWS resources from VPC, subnets, routing tables, security groups and EC2 instances. 
 * After script is created:
     - `terraform plan` checks script 
     - `terraform apply` to create instance
@@ -27,4 +28,156 @@
 * ingress for inbound rules 
 * egress for outbound rules
 
-#### 
+#### Creation of VPC, Subnets, Route Tables and deploying instances
+
+##### VPC
+```
+# Launching a VPC using terraform
+resource "aws_vpc" "eng99_raj_vpc" {
+    cidr_block = "10.0.0.0/16" # "10.0.0.0/16"
+    tags = {
+        Name = "eng99_raj_terraform_vpc"
+    }
+}
+```
+
+##### Internet Gateway
+```
+# Create Internet Gateway
+resource "aws_internet_gateway" "IGW" {
+    vpc_id = aws_vpc.eng99_raj_vpc.id
+    tags = {
+        Name = "eng99_raj_terraform_IG"
+    }
+}
+```
+
+##### Public Subnet
+```
+# Create a Public Subnet
+resource "aws_subnet" "publicsubnet" {
+    vpc_id = aws_vpc.eng99_raj_vpc.id
+    cidr_block = "10.0.7.0/24"
+    tags = {
+      Name = "eng99_raj_terraform_publicSN"
+    }
+}
+```
+
+##### Private Subnet
+```
+# Private Subnet
+resource "aws_subnet" "privatesubnet"{
+  vpc_id = aws_vpc.eng99_raj_vpc.id
+  cidr_block = "10.0.8.0/24"
+  tags = {
+    Name = "eng99_raj_terraform_privateSN"
+  }
+}
+```
+
+##### Route Table
+```
+# Create a route table
+resource "aws_route_table" "PublicRT" {
+    vpc_id = aws_vpc.eng99_raj_vpc.id
+         route {
+            cidr_block = "0.0.0.0/0"
+            gateway_id = aws_internet_gateway.IGW.id
+        }
+    tags = {
+      Name = "eng99_raj_terraform_RT"
+    }
+}
+```
+
+```
+# Route Table association
+resource "aws_route_table_association" "PublicRTassociation" {
+    subnet_id = aws_subnet.publicsubnet.id
+    route_table_id = aws_route_table.PublicRT.id
+}
+```
+
+##### App Security Group
+```
+# App EC2 Security Groups
+resource "aws_security_group" "allow_tls" {
+  name        = "eng99_raj_terraform_app_SG"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = aws_vpc.eng99_raj_vpc.id
+
+  ingress {
+    description      = "access the app from anywhere world"
+    from_port        = 3000
+    to_port          = 3000
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+  ingress {
+    description      = "ssh from world"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
+
+```
+
+##### EC2 Instance
+```
+# launching an app EC2 Instance using Terraform
+resource "aws_instance" "app_instance" {
+    # add the ami id for 18.04 LTS
+    ami = "ami-07d8796a2b0f8d29c"
+
+    # choose t2.micro
+    instance_type = "t2.micro"
+
+    # Enable public IP for app instance
+    associate_public_ip_address = true
+
+    # Subnet selection
+    subnet_id = aws_subnet.publicsubnet.id
+    
+    # Security Group
+    vpc_security_group_ids = [aws_security_group.allow_tls.id]
+
+    # add tags for Name
+    tags = {
+        Name = var.name
+    }
+
+    key_name = "eng99" # ensure that we have this key in .ssh folder
+}
+```
--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@
 
 # AWS plugins/dependencies will be downloaded
 provider "aws" {
-    region = var.region # eu-west-1
+    region = "eu-west-1" # eu-west-1
     # This will allow terraform to create services on eu-west-1
 
 
@@ -10,7 +10,7 @@ provider "aws" {
 
 # Launching a VPC using terraform
 resource "aws_vpc" "eng99_raj_vpc" {
-    cidr_block = var.vpc_cidr # "10.0.0.0/16"
+    cidr_block = "10.0.0.0/16" # "10.0.0.0/16"
     tags = {
         Name = "eng99_raj_terraform_vpc"
     }
@@ -27,16 +27,16 @@ resource "aws_internet_gateway" "IGW" {
 # Create a Public Subnet
 resource "aws_subnet" "publicsubnet" {
     vpc_id = aws_vpc.eng99_raj_vpc.id
-    cidr_block = var.publicSN_CIDR
+    cidr_block = "10.0.7.0/24"
     tags = {
-      Name = var.aws_public_subnet
+      Name = "eng99_raj_terraform_publicSN"
     }
 }
 
 # Private Subnet
 resource "aws_subnet" "privatesubnet"{
   vpc_id = aws_vpc.eng99_raj_vpc.id
-  cidr_block = var.privateSN_CIDR
+  cidr_block = "10.0.8.0/24"
   tags = {
     Name = "eng99_raj_terraform_privateSN"
   }
@@ -46,7 +46,7 @@ resource "aws_subnet" "privatesubnet"{
 resource "aws_route_table" "PublicRT" {
     vpc_id = aws_vpc.eng99_raj_vpc.id
          route {
-            cidr_block = var.IG_CIDR
+            cidr_block = "0.0.0.0/0"
             gateway_id = aws_internet_gateway.IGW.id
         }
     tags = {
@@ -55,12 +55,8 @@ resource "aws_route_table" "PublicRT" {
 }
 
 # Private Subnet Routing Table
-resource "aws_route_table" "PrivateRT"{
+resource "aws_route_table" "PrivateRT" {
   vpc_id = aws_vpc.eng99_raj_vpc.id
-  route {
-    cidr_block = var.IG_CIDR
-    gateway_id = aws_internet_gateway.IGW.id 
-  }
   tags = {
     Name = "eng99_raj_terraform_private_RT"
   }
@@ -80,7 +76,7 @@ resource "aws_route_table_association" "PrivateRTassociation" {
 
 # App EC2 Security Groups
 resource "aws_security_group" "allow_tls" {
-  name        = "eng99_raj_terraform"
+  name        = "eng99_raj_terraform_app_SG"
   description = "Allow TLS inbound traffic"
   vpc_id      = aws_vpc.eng99_raj_vpc.id
 
@@ -148,7 +144,7 @@ resource "aws_security_group" "db_sg" {
     from_port = 22
     to_port = 22
     protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["10.0.7.0/24"]
   }
 
   egress {
@@ -163,12 +159,92 @@ resource "aws_security_group" "db_sg" {
   }
 
 }
+
+# Security Group for NAT instance
+resource "aws_security_group" "nat_sg" {
+  name = "eng99_raj_terraform_nat_sg"
+  description = "NAT Security Group"
+  vpc_id = aws_vpc.eng99_raj_vpc.id
+  
+  ingress {
+    description = "HTTPS"
+    from_port = 443
+    to_port = 443
+    protocol = "tcp"
+    cidr_blocks = ["10.0.0.0/24"]
+  }
+
+  ingress {
+    description = "All TCP"
+    from_port = 0
+    to_port = 65535
+    protocol = "tcp"
+    cidr_blocks = ["10.0.0.0/24"]
+  }
+
+  ingress {
+    description = "ssh"
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow Ping"
+    from_port = 8
+    to_port = 0
+    protocol = "icmp"
+    cidr_blocks = ["10.0.0.0/24"]
+  }
+
+  ingress {
+    description = "HTTP"
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "ssh"
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["10.0.0.0/24"]
+  }
+
+  egress {
+    description = "HTTP"
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "Allow Ping"
+    from_port = 8
+    to_port = 0
+    protocol = "icmp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "HTTPS"
+    from_port = 443
+    to_port = 443
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+}
    
 
 # launching an app EC2 Instance using Terraform
 resource "aws_instance" "app_instance" {
     # add the ami id for 18.04 LTS
-    ami = var.app_ami_id
+    ami = "ami-07d8796a2b0f8d29c"
 
     # choose t2.micro
     instance_type = "t2.micro"
@@ -187,16 +263,16 @@ resource "aws_instance" "app_instance" {
         Name = var.name
     }
 
-    key_name = var.aws_key_name # ensure that we have this key in .ssh folder
+    key_name = "eng99" # ensure that we have this key in .ssh folder
 }
 
 # launching db instance using terraform
 resource "aws_instance" "db_instance" {
-  ami = var.app_ami_id
+  ami = "ami-07d8796a2b0f8d29c"
 
   instance_type = "t2.micro"
 
-  associate_public_ip_address = true
+  associate_public_ip_address = false 
 
   subnet_id = aws_subnet.publicsubnet.id
 
@@ -206,7 +282,26 @@ resource "aws_instance" "db_instance" {
     Name = "eng99_raj_terraform_db_instance"
   }
 
-  key_name = var.aws_key_name
+  key_name = "eng99"
+}
+
+# NAT Instance
+resource "aws_instance" "nat_instance" {
+  ami = "ami-00b9ad31459a9dcdf"
+
+  instance_type = "t2.micro"
+
+  associate_public_ip_address = true
+
+  subnet_id = aws_subnet.publicsubnet.id
+
+  vpc_security_group_ids = [aws_security_group.nat_sg.id]
+
+  tags = {
+    Name = "eng99_raj_terraform_nat"
+  }
+
+  key_name = "eng99"
 }
 
 # To initialise we use terraform init
